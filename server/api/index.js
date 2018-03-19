5c80786bb37fef81da6d3d66604e333c61059382
Dongwoo Kang <schach0415@yahoo.com>
2018-03-19 17:54:07 -0400

    some error fixed

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'));
 router.use('/establishments', require('./establishments'));
-router.use('/checkin', require('./checkin'));
+router.use('/checkins', require('./checkins'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
