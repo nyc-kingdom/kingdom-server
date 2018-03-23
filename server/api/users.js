9cdee910d71ea45e453e217de401fed28a125c4a
Dongwoo Kang <schach0415@yahoo.com>
2018-03-23 18:17:39 -0400

    user now has kingdom when signin

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User, Kingdom } = require('../db/models')
+const asyncHandler = require('express-async-handler')
+const flickr = process.env.FLICKR_API_KEY
+const googleMap = process.env.GOOGLE_MAP
+const axios = require('axios')
+
 module.exports = router
 
 router.get('/', (req, res, next) => {
@@ -20,3 +25,20 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
+router.put('/:id', asyncHandler(async (req, res, next) => {
+  const userId = +req.params.id;
+  const { address } = req.body
+  const googleCoords = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleMap}`)
+  const { lat, lng } = googleCoords.data.results[0].geometry.location
+  const flckr =  await axios.get(`https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=${flickr}&lat=${lat}&lon=${lng}&format=json&nojsoncallback=1`)
+  const kingdomName = flckr.data.places.place[0].woe_name
+  const kingdom = await Kingdom.scope('populated').findOrCreate({
+    where: { name: kingdomName },
+  });
+  await User.update(
+    { kingdomId: kingdom[0].dataValues.id },
+    { where: { id: userId } }
+  );
+  const updated = await User.scope('populated').findById(userId)
+  res.json(updated);
+}))
